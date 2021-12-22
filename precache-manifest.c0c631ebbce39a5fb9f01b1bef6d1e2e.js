527a572b70ae052aae4dfae466defe7afc391b2c
Mstars_lab1_20 <mstarsacademy@MStarss-iMac-1.local>
2021-12-22 10:46:26 +0800

    Updates

---
+++ precache-manifest.c0c631ebbce39a5fb9f01b1bef6d1e2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "305c4cb0ef5e348dc341",
+    "url": "/tsedevee/course-directory.git/static/css/main.e86dd9d4.chunk.css"
+  },
+  {
+    "revision": "305c4cb0ef5e348dc341",
+    "url": "/tsedevee/course-directory.git/static/js/main.305c4cb0.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/tsedevee/course-directory.git/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "656ff212b078dfeeda88",
+    "url": "/tsedevee/course-directory.git/static/js/runtime~main.656ff212.js"
+  },
+  {
+    "revision": "03c34ce0c5629d306377097f3be20375",
+    "url": "/tsedevee/course-directory.git/index.html"
+  }
+];
